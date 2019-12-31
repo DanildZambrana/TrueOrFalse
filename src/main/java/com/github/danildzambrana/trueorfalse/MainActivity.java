2d6faec956a3ee234952202463ce1035372b683d
Danild <danildjohan12@gmail.com>
2019-12-31 01:04:30 -0600

    Primer commit

---
+++ src/main/java/com/github/danildzambrana/trueorfalse/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019.
+ * This file is subject to the terms and conditions defined in file 'LICENSE.md', which is part of this source code package.
+ */
+
+package com.github.danildzambrana.trueorfalse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ src/test/java/com/github/danildzambrana/trueorfalse/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019.
+ * This file is subject to the terms and conditions defined in file 'LICENSE.md', which is part of this source code package.
+ */
+
+package com.github.danildzambrana.trueorfalse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
